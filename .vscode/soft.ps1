c74d37bed1059c896fdf7af7400b7128979b858c
Eduardo Herráez <eherraez@afi.es>
2023-09-17 03:22:10 +0200

    Añadido Control google

--- .vscode/soft.ps1
+++ .vscode/soft.ps1
@@ -1,71 +1,59 @@
-#soft
-Function connectEOL {
-    Clear-Host
-    $connect =Read-Host "Do you want to connect to Exchange Online? Y/(n)"
-    switch ($connect) {
-        'Y' { Connect-ExchangeOnline
-             Return }
-        default { write-host "not connected" }
-    }
-}
+
 function ShowMainMenu {
+# main menu 
     $choice = $null
-    
-    do {
+        do {
         Clear-Host
-        checkeol
-        showgam
-        Write-Host "Main Menu"
+        checkeol #check if connected to exchange online
+        showgam #check if gam is working
+        Write-Host "Main Menu" 
         Write-Host "1. User tools"
         Write-Host "2. Group tools"
         Write-Host "3. Connect to Exchange online"
         Write-Host "4. Tool Settings"
         Write-Host "Q. Quit"
-
         $choice = Read-Host "Please select an option"
-
         switch ($choice) {
             '1' { ShowUserMenu }
-            '2' { Write-Host "Hello from the main menu!" }
+            '2' { ShowGroupMenu}
             '3' { connectEOL  }
             '4' { settingsmenu  }
             'Q' { return }
             default { Write-Host "Invalid choice, please try again." }
         }
-
-#        PauseForUser
-
+        #        PauseForUser
     } while ($choice -ne 'Q')
 }
 
 function settingsmenu {
     $choice = $null
-    
-    do {
+        do {
         Clear-Host
-        checkeol
-        showgam
+        checkeol #check if connected to exchange online
+        showgam #check if gam is working
         Write-Host "Setting Menu"
         Write-Host "1. Set Google to 365 routing group"
         Write-Host "2. Set 365 to google Group tools"
         Write-Host "Q. Quit"
-
         $choice = Read-Host "Please select an option"
-
         switch ($choice) {
             '1' { }
             '2' { }
             'Q' { return }
             default { Write-Host "Invalid choice, please try again." }}
-        
-    
-
-#        PauseForUser
-        
-    } while ($choice -ne 'Q') }
+     } while ($choice -ne 'Q') }
 
 
-
+    Function connectEOL {
+        #function to connect to exchange online
+        Clear-Host
+        $connect =Read-Host "Do you want to connect to Exchange Online? Y/(n)"
+        switch ($connect) {
+            'Y' { Connect-ExchangeOnline
+                 Return }
+            default { write-host "not connected" }
+        }
+    }
 function ShowUserMenu {
     $choice = $null
     do {
@@ -82,14 +70,10 @@ function ShowUserMenu {
 
         switch ($choice) {
 
-            '1' {
-                Clear-Host
-                checkeol
-                showgam
-                $Username = read-host "enter username to process"
-                checkExistoffice365 -userinput $Username
-                ShowUserSingleMenu -userinput $Username
-             }
+            '1' {clear-host
+                 $username=askforuser 
+                 ShowUserSingleMenu -userInput $Username
+                 } #show single user menu
             '2' { Write-Host ([Environment]::UserName) }
             'B' { return }
             'Q' { Exit }
@@ -100,6 +84,31 @@ function ShowUserMenu {
 
     } while ($choice -ne 'B')
 }
+function askforuser{
+    $Username = read-host "enter username to process"
+    $check=checkExistoffice365 -userinput $Username #check if user exists in office 365
+    if ($check -eq $null) {
+    CheckexistsGoogle -userInput $Username
+    Write-Host "The variable is null."
+    Write-Host "username" $username
+    Write-Host "check" $check
+    PauseForUser
+} else {
+    Write-Host "The variable is not null."
+    Write-Host "username" $username
+    Write-Host "check" $check
+    pauseforuser
+}
+
+    # if checkExistoffice365 -is null call checkexistsGoogle
+
+    
+
+    
+  
+    #CheckexistsGoogle -userinput $Username #check if user exists in google workspace
+return $Username}
+
 
 function ShowUserSingleMenu {
     param (
@@ -108,8 +117,8 @@ function ShowUserSingleMenu {
     $choice = $null
     do {
         Clear-Host
-        checkeol
-        showgam
+        checkeol #check if connected to exchange online
+        showgam #check if gam is working
         
         Write-Host "current User : "-NoNewline
         Write-Host $userinput -ForegroundColor Green
@@ -189,44 +198,58 @@ function showGam {
     $output = & gam info domain 2>&1
  showgam -show $output
   }
+
+  function CheckexistsGoogle{
+    param (
+        [string] $userInput
+    )
+    $userToCheck = $userInput
+    # Check if the user exists using GAM
+try {
+    $output = & gam info user $userToCheck 2>&1
+    if ($output -like "*Error:*") {
+        Write-Host "$userToCheck does not exist." -ForegroundColor Red
+        #call again a askforuser function and try again
+        $userToCheck = askforuser
+      #  CheckexistsGoogle -userInput $userToCheck
+
+        
+    } else {
+        Write-Host "$userToCheck exists. "
+        #call again a askforuser function and try again sugessting try with simiral address
+       
+
+      
+    }
+} catch {
+    Write-Host "An error occurred: $($_.Exception.Message)"
+}
+  #function to check whit gam if a user exists in google workspace
+}
+
+
 function checkExistoffice365 {
     param (
         [string] $userInput
     )
-    
-        try {
-        # We'll try to retrieve just the UserPrincipalName to make the query lightweight
+            try {
+        # We'll try to retrieve just the alias
         $userUPN = Get-Mailbox -Filter "alias -eq '$userInput'" -ErrorAction Stop | Select-Object -ExpandProperty alias
-    
         if ($userUPN) {
-          
+            Write-Host "$userInput exists in Exchange Online." -foreground Green
+               
         } else {
-            Write-Host "$userInput does not exist."
-            $userInput = $null
-            write-host "quieres intentarlo con la dirección de correo?"
-             try {
-                                $email=read-host "mete el email"
-                                  # We'll try to retrieve just the UserPrincipalName to make the query lightweight
-                                $userUPN = Get-Mailbox -Filter "UserPrincipalName  -eq '$email'" -ErrorAction Stop | Select-Object -ExpandProperty UserPrincipalName
-                    
-                            if ($userUPN) {
-                                                        } else {
-                                Write-Host "$email does not exist."
-
-                                }
-    
-                            } catch {
-                            Write-Host "$userToCheck does not exist."
-                            $userInput = $null
-                            }
-    
-    
-        }
+            Write-Host "$userInput does not exist in office 365."
+            #if does not exist the function return a null value
+            
+            return $null
+       }
+       return $userInput
     } catch {
-        Write-Host "$userToCheck does not exist."
+        Write-Host "$userInput does not exist."
     }
-    
 }
+       
 function PauseForUser {
     Write-Host "Press any key to continue..."
     $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
@@ -266,8 +289,23 @@ function UserMigrationStatus{
         [string] $userInput
     )
     # chequear tamaño buzon
-    Get-MailboxStatistics $userInput  | format-table  TotalItemSize
-    Get-MailboxStatistics $userInput -Archive | Format-Table TotalItemSize
+   # $userUPN = Get-Mailbox -Filter "alias -eq '$userInput'" -ErrorAction Stop | Select-Object -ExpandProperty alias
+ #Get-MailboxStatistics $userInput  | Select-Object  TotalItemSize
+#   Get-MailboxStatistics $userInput  | format-table  TotalItemSize
+$mailboxtotalSize = (Get-MailboxStatistics $userInput | Select-Object -ExpandProperty TotalItemSize)
+$archivetotalSize = (Get-MailboxStatistics $userInput -Archive| Select-Object -ExpandProperty TotalItemSize)
+$retentionPolicy = (Get-Mailbox -Identity $userInput).RetentionPolicy
+
+if ($null -eq $retentionPolicy) {
+    Write-Host "No retention policy is set for $userInput." -foreground green
+} else {
+    Write-Host "Retention policy for $userInput is: $retentionPolicy" -foreground red
+}
+
+write-host "Tamaño del buzon principal:" $mailboxtotalSize
+write-host "Tamaño del buzon de Archivo:" $archivetotalSize
+
+ #   Get-MailboxStatistics $userInput -Archive | Format-Table TotalItemSize
     # chequear archivo
     showarchive -userInput $userInput 
 
